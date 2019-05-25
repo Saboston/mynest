70f366f4ac4915cc5934b741a849cfc75ad7d0a9
zhangjie <657955101@qq.com>
2019-05-25 19:09:47 +0800

    1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('aaa')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  // @Get()
+  // getBye(): string {
+  //   return `byebye`;
+  // }
 
-  @Get('rest')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getHello(@Param('id') id: string) {
+
+    return this.appService.getHello()+`${id}`+``;
   }
+
 }
