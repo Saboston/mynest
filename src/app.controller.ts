b69a0849dd13a82043d571963d93732844b7847a
zj66golden <zj66golden@gmail.com>
2019-05-26 22:43:03 +0800

    1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 @Controller('aaa')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
   @Get('hello')
   getHello(): string {
     return this.appService.getHello();
