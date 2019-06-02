30bdec23d096fe94fe1eb5b4a2aced52c6aa397e
zj66golden <zj66golden@gmail.com>
2019-06-02 22:19:06 +0800

    12

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Auth } from './auth.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(Auth)
+    private readonly authRepository: Repository<Auth>,
+  ) {}
+
+   findAll(): Promise<Auth[]> {
+    return  this.authRepository.find();
+  }
+}
