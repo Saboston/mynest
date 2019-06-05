55d661b09d99d17ddf2b5908d409c3d3377bc03b
zhangjie <657955101@qq.com>
2019-06-05 18:51:16 +0800

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,19 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { Connection } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { mysqlConfig } from './config/database'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(mysqlConfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '188.131.168.95',
+      port: 3306,
+      username: 'root',
+      password: 'golden66',
+      database: 'mynest',
+      entities: ['src/entity/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     AuthModule
   ],
   controllers: [AppController],
